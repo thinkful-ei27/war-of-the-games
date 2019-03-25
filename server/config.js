5886d85932418a493c69070a48ab4d5ad84d7b85
John Young <john.young@spreadstreet.io>
2019-03-25 09:34:53 -0400

    add .travis.yml

--- server/config.js
+++ server/config.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://dev:therock1@ds139775.mlab.com:39775/spaced-prod',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://dev:therock1@ds139775.mlab.com:39775/spaced-prod',
+  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://dev:password123@ds123196.mlab.com:23196/final-capstone-dev',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://dev:password123@ds123196.mlab.com:23196/final-capstone-dev',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
   // DATABASE_URL:
