317612977ca7ed5797d61d66739486d435371c52
Derek Houck <derek@derekhouck.com>
2019-03-26 12:32:40 -0700

    Add randomness to GET /api/games/battle

--- server/routes/games.js
+++ server/routes/games.js
@@ -3,6 +3,11 @@ const Game = require("../models/game");
 
 const router = express.Router();
 
+const findRandGame = count => {
+  const rand = Math.floor(Math.random() * count);
+  return Game.findOne().skip(rand);
+};
+
 router.get("/", (req, res, next) => {
   Game.find()
     .sort({ name: "asc" })
@@ -11,8 +16,10 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/battle", (req, res, next) => {
-  Game.find()
-    .limit(2)
+  return Game.countDocuments()
+    .then(count => {
+      return Promise.all([findRandGame(count), findRandGame(count)]);
+    })
     .then(results => res.json(results))
     .catch(err => next(err));
 });
