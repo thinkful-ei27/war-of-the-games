0828e176ef2934b9e472ed4d4553a7b6bf0479a0
Tyler Crabb <tydacrabb@gmail.com>
2019-03-26 12:47:41 -0700

    serialized vote

--- client/src/actions/gameActions.js
+++ client/src/actions/gameActions.js
@@ -1,11 +1,11 @@
-import { API_BASE_URL } from '../config'
-import axios from 'axios';
+import axios from "axios";
+import { API_BASE_URL } from "../config";
 
-export const FETCH_GAMES = 'FETCH_GAMES';
+export const FETCH_GAMES = "FETCH_GAMES";
 
-export const FETCH_GAMES_SUCCESS = 'FETCH_GAMES_SUCCESS';
+export const FETCH_GAMES_SUCCESS = "FETCH_GAMES_SUCCESS";
 
-export const FETCH_GAMES_ERROR = 'FETCH_GAMES_ERROR';
+export const FETCH_GAMES_ERROR = "FETCH_GAMES_ERROR";
 
 export const fetchGamesSuccess = games => ({
   type: FETCH_GAMES_SUCCESS,
@@ -14,8 +14,8 @@ export const fetchGamesSuccess = games => ({
 
 export const fetchGames = () => (dispatch, getState) => {
   axios({
-    url: 'our server url',
-    method: 'GET'
+    url: "our server url",
+    method: "GET"
   })
     .then(response => {
       dispatch(fetchGamesSuccess(response.data));
@@ -23,4 +23,24 @@ export const fetchGames = () => (dispatch, getState) => {
     .catch(err => {
       console.error(err);
     });
-}
+};
+
+export const HANDLE_VOTE = "HANDLE_VOTE";
+
+export const handleVote = (game1, game2, userChoice) => (
+  dispatch,
+  getState
+) => {
+  axios
+    .post("url", {
+      game1,
+      game2,
+      userChoice
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
