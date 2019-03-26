156bf1c71e02ff88acd796316ff9f5a4773abe8f
Evan Guirino <etg624@gmail.com>
2019-03-26 13:10:55 -0700

    drop down looks alright for now

---
+++ client/src/components/MenuButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MenuButton() {
+  return <i className="fas fa-bars fa-2x" />;
+}
+
+export default MenuButton;
--- client/src/components/header-bar.js
+++ client/src/components/header-bar.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { decorator as reduxBurgerMenu } from 'react-burger-menu';
+import MenuButton from './MenuButton';
 import { slide as Menu } from 'react-burger-menu';
+import './header.css';
 import { clearAuth } from '../actions/auth';
 import { clearAuthToken } from '../local-storage';
 
-export class HeaderBar extends React.Component {
+export class HeaderBar extends Component {
+  state = {
+    dropdownCollapsed: false
+  };
+
   logOut() {
     // eslint-disable-next-line react/destructuring-assignment
     this.props.dispatch(clearAuth());
     clearAuthToken();
+  }
+  toggleNav() {
+    this.setState({
+      dropdownCollapsed: !this.state.dropdownCollapsed
+    });
   }
 
   render() {
@@ -23,13 +35,33 @@ export class HeaderBar extends React.Component {
         </button>
       );
     }
+
     return (
-      <Menu className="header-bar">
-        <Link to="/about">About</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/leaderBoard">Leader Board</Link>
-        {logOutButton}
-      </Menu>
+      <header className="header">
+        <Menu
+          className="bm-burger-button"
+          right
+          customBurgerIcon={<MenuButton />}
+          onStateChange={() => this.toggleNav}
+          isOpen={false}
+          width={'10%'}
+          height={'10%'}
+        >
+          <Link to="/" onClick={() => this.toggleNav()}>
+            Home
+          </Link>
+          <Link to="/about" onClick={() => this.toggleNav()}>
+            About
+          </Link>
+          <Link to="/profile" onClick={() => this.toggleNav()}>
+            Profile
+          </Link>
+          <Link to="/leaderBoard" onClick={() => this.toggleNav()}>
+            Leader Board
+          </Link>
+          {logOutButton}
+        </Menu>
+      </header>
     );
   }
 }
