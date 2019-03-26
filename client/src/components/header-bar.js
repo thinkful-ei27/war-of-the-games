ee84910dad585f419f3d834a05ec19e8a14bb90b
Evan Guirino <etg624@gmail.com>
2019-03-26 11:39:30 -0700

    drop down started

--- client/src/components/header-bar.js
+++ client/src/components/header-bar.js
@@ -1,33 +1,41 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {clearAuth} from '../actions/auth';
-import {clearAuthToken} from '../local-storage';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu';
+import { clearAuth } from '../actions/auth';
+import { clearAuthToken } from '../local-storage';
 
 export class HeaderBar extends React.Component {
-    logOut() {
-        this.props.dispatch(clearAuth());
-        clearAuthToken();
-    }
+  logOut() {
+    // eslint-disable-next-line react/destructuring-assignment
+    this.props.dispatch(clearAuth());
+    clearAuthToken();
+  }
 
-    render() {
-        // Only render the log out button if we are logged in
-        let logOutButton;
-        if (this.props.loggedIn) {
-            logOutButton = (
-                <button onClick={() => this.logOut()}>Log out</button>
-            );
-        }
-        return (
-            <div className="header-bar">
-                <h1>Foo App</h1>
-                {logOutButton}
-            </div>
-        );
+  render() {
+    const { loggedIn } = this.props;
+    // Only render the log out button if we are logged in
+    let logOutButton;
+    if (loggedIn) {
+      logOutButton = (
+        <button type="button" onClick={() => this.logOut()}>
+          Log out
+        </button>
+      );
     }
+    return (
+      <Menu className="header-bar">
+        <Link to="/about">About</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/leaderBoard">Leader Board</Link>
+        {logOutButton}
+      </Menu>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    loggedIn: state.auth.currentUser !== null
+  loggedIn: state.auth.currentUser !== null
 });
 
 export default connect(mapStateToProps)(HeaderBar);
