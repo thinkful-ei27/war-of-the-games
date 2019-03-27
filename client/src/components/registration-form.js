17115325746efbe247c03beba1d753ae007aff8c
Evan Guirino <etg624@gmail.com>
2019-03-27 12:18:28 -0700

    added labels as props to form fields

--- client/src/components/registration-form.js
+++ client/src/components/registration-form.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { Field, reduxForm, focus } from "redux-form";
-import { registerUser } from "../actions/users";
-import { login } from "../actions/auth";
-import Input from "./input";
-import { required, nonEmpty, matches, length, isTrimmed } from "../validators";
+import React from 'react';
+import { Field, reduxForm, focus } from 'redux-form';
+import { registerUser } from '../actions/users';
+import { login } from '../actions/auth';
+import Input from './input';
+import { required, nonEmpty, matches, length, isTrimmed } from '../validators';
 
 const passwordLength = length({ min: 10, max: 72 });
-const matchesPassword = matches("password");
+const matchesPassword = matches('password');
 
 export class RegistrationForm extends React.Component {
   onSubmit(values) {
@@ -25,29 +25,41 @@ export class RegistrationForm extends React.Component {
         className="login-form"
         onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
       >
-        <label htmlFor="firstName">First name</label>
-        <Field component={Input} type="text" name="firstName" />
-        <label htmlFor="lastName">Last name</label>
-        <Field component={Input} type="text" name="lastName" />
-        <label htmlFor="username">Username</label>
+        <Field
+          component={Input}
+          label="first name"
+          type="text"
+          name="firstName"
+        />
+
+        <Field
+          component={Input}
+          label="last name"
+          type="text"
+          name="lastName"
+        />
+
         <Field
           component={Input}
           type="text"
           name="username"
+          label="username"
           validate={[required, nonEmpty, isTrimmed]}
         />
-        <label htmlFor="password">Password</label>
+
         <Field
           component={Input}
           type="password"
           name="password"
+          label="password"
           validate={[required, passwordLength, isTrimmed]}
         />
-        <label htmlFor="passwordConfirm">Confirm password</label>
+
         <Field
           component={Input}
           type="password"
           name="passwordConfirm"
+          label="confirm password"
           validate={[required, nonEmpty, matchesPassword]}
         />
         <button
@@ -62,7 +74,7 @@ export class RegistrationForm extends React.Component {
 }
 
 export default reduxForm({
-  form: "registration",
+  form: 'registration',
   onSubmitFail: (errors, dispatch) =>
-    dispatch(focus("registration", Object.keys(errors)[0]))
+    dispatch(focus('registration', Object.keys(errors)[0]))
 })(RegistrationForm);
