1dcb3f2b5c0a9181a18c5fd761e998811f895efe
Tyler Crabb <tydacrabb@gmail.com>
2019-03-27 10:46:14 -0700

    prompt

--- client/src/components/signupPrompt.js
+++ client/src/components/signupPrompt.js
@@ -8,7 +8,7 @@ export class SignupPrompt extends React.Component {
   render() {
     return (
       <div className="sign-up-prompt">
-        <header className="sign-up-prompt-header">Sign Up to Keep Voting!</header>
+        <header className="sign-up-prompt-header">Sign up Bitch!</header>
         <Link to="/register">
           <button
             id="sign-up-prompt-btn"
