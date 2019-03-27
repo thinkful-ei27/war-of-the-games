5c2acf8f87379bd778dd7a3a655d4af46bf9a1e7
Evan Guirino <etg624@gmail.com>
2019-03-26 19:11:57 -0700

    styled header

--- client/src/components/header-bar.js
+++ client/src/components/header-bar.js
@@ -1,7 +1,9 @@
-import React from "react";
-import { connect } from "react-redux";
-import { clearAuth } from "../actions/auth";
-import { clearAuthToken } from "../local-storage";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { clearAuth } from '../actions/auth';
+import { clearAuthToken } from '../local-storage';
+import './styles/header.css';
 
 export class HeaderBar extends React.Component {
   logOut() {
@@ -15,11 +17,27 @@ export class HeaderBar extends React.Component {
     if (this.props.loggedIn) {
       logOutButton = <button onClick={() => this.logOut()}>Log out</button>;
     }
+    const about = <Link to="/about">About</Link>;
     return (
-      <div className="header-bar">
-        <h1>Foo App</h1>
+      <header className="header-bar">
+        <div className="nes-container with-title is-centered">
+          <h1 className="title">Foo App</h1>
+          <p>Some description here?</p>
+          <div className="nes-container nav-container">
+            <Link className="nav-button" to="/">
+              About
+            </Link>
+            <Link className="nav-button" to="/register">
+              Sign Up
+            </Link>
+            <Link className="nav-button" to="/vote">
+              Vote
+            </Link>
+          </div>
+        </div>
+
         {logOutButton}
-      </div>
+      </header>
     );
   }
 }
