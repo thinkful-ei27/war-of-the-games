75237b741b2681539259527afae77d98c68e21ec
Peter Ramos <peterdramos02@gmail.com>
2019-03-28 12:02:05 -0400

    vote stats is now dynamic

--- client/src/components/vote-stats.js
+++ client/src/components/vote-stats.js
@@ -3,22 +3,14 @@ import VoteCard from "./vote-card";
 import "./styles/vote-stats.css";
 
 export default function VoteStats(props) {
-  const dummyData = {
-    name: "Street Fighter II",
-    coverUrl: "https://images.igdb.com/igdb/image/upload/t_720p/co1hq8.jpg",
-    percent: "69%"
-  };
   const { feedback } = props;
-  const { name, coverUrl, percent } = dummyData;
-  const { data } = feedback;
-  // const { percentage } = data;
+  const { percentage, name, coverUrl } = feedback;
 
   return (
     <div className="vote-stats-container">
       <VoteCard name={name} src={coverUrl} />
-      {/* <VoteCard name={gameTwo.name} src={gameTwo.coverUrl} /> */}
       <p className="voting-feedback">
-        {`${name} has won ${percent} of all battles`}
+        {`${name} has won ${percentage * 100}% of all battles`}
       </p>
     </div>
   );
