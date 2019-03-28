96cccd0675f70908d3f5ab1a913ccc51d69889e5
Peter Ramos <peterdramos02@gmail.com>
2019-03-28 12:06:33 -0400

    percent value in voteCard shows only integers now w/ some pasrseInt magic and text-align centerd text in vvoteStats container

--- client/src/components/vote-stats.js
+++ client/src/components/vote-stats.js
@@ -6,11 +6,13 @@ export default function VoteStats(props) {
   const { feedback } = props;
   const { percentage, name, coverUrl } = feedback;
 
+  const percent = parseInt(Number(percentage) * 100, 10);
+
   return (
     <div className="vote-stats-container">
       <VoteCard name={name} src={coverUrl} />
       <p className="voting-feedback">
-        {`${name} has won ${percentage * 100}% of all battles`}
+        {`${name} has won ${percent}% of all battles`}
       </p>
     </div>
   );
