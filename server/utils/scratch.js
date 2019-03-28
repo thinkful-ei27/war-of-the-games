b5011438a605c0e8529cee8e582b5440a3373d2c
John Young <john.young@spreadstreet.io>
2019-03-28 09:36:49 -0400

    Added keys to env file

--- server/utils/scratch.js
+++ server/utils/scratch.js
@@ -1,5 +1,11 @@
 'use strict';
-// const { IGDB_KEY1, IGDB_KEY2, IGDB_KEY3, IGDB_KEY4, IGDB_KEY5 } = process.env;
+const { 
+  IGDB_KEY1, 
+  IGDB_KEY2, 
+  IGDB_KEY3, 
+  IGDB_KEY4, 
+  IGDB_KEY5 
+} = require('../config');
 
 const igdbApi = require('../utils/gameApi');
 
