6cddfd360643fbedd23f6c3b3ee2f2225b5fa189
Evan Guirino <etg624@gmail.com>
2019-03-29 07:50:00 -0700

    basic/static profile page

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import requiresLogin from './requires-login';
-import { fetchProtectedData } from '../actions/protected-data';
+import './styles/profile.css';
 
 export class ProfilePage extends React.Component {
   componentDidMount() {
@@ -13,9 +13,29 @@ export class ProfilePage extends React.Component {
     console.log(this.props);
     return (
       <div className="dashboard">
-        <div className="dashboard-username">Username: {username}</div>
-        <div className="dashboard-name">Name: {name}</div>
-        <div className="dashboard-protected-data" />
+        <div className="nes-container with-title profile-info-container">
+          <p className="title user">Hello {name}!</p>
+          <section className="personal-info">
+            <div className="nes-container with-title is-rounded about-me-container">
+              <p className="title">
+                <img
+                  className="title profile-pic"
+                  src="https://i.pinimg.com/originals/2f/56/20/2f5620472cc9033a970e3b0bd4fa66d7.png"
+                  alt="profile-pic"
+                />
+              </p>
+              <p className="about-me">
+                Bacon ipsum dolor amet strip steak filet mignon capicola,
+                picanha boudin pig frankfurter shank kielbasa tri-tip pancetta.
+                Frankfurter shoulder swine picanha pig. Tongue ribeye pig strip
+                steak brisket, ham shankle pork chop doner jowl turducken cow
+                tenderloin frankfurter t-bone. Ribeye pastrami filet mignon
+                burgdoggen. Tri-tip corned beef beef kevin drumstick. Cow
+                picanha alcatra tail meatloaf.{' '}
+              </p>
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
