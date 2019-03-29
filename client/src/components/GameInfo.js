4180e6021a10f4813bcc6221d8e0673d2394fa26
Peter Ramos <peterdramos02@gmail.com>
2019-03-28 21:45:51 -0400

    gets rid of most webPack warnings and destructure loddegIn prop in app.js

--- client/src/components/GameInfo.js
+++ client/src/components/GameInfo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./styles/gameInfo.css";
 import { connect } from "react-redux";
 
