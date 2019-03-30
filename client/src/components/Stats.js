a8503aa4d148ea7bad57809aedff3bcb91121d3f
John Young <john.young@spreadstreet.io>
2019-03-29 21:44:29 -0400

    Updated top five and bottom 5 into all

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import axios from "axios";
 import { API_BASE_URL } from "../config";
 import MappedTable from "./MappedTable";
+import "./styles/gameInfo.css";
 
 export default class Stats extends Component {
   constructor() {
     super();
     this.state = {
-      all: []
+      all: [],
+      topFive: [],
+      bottomFive: []
     };
   }
 
@@ -15,8 +18,16 @@ export default class Stats extends Component {
     const response = await axios.get(`${API_BASE_URL}/history/all`);
     // const json = await response.json();
     const { data } = response;
-    data.sort((a, b) => parseFloat(b.percentage) - parseFloat(a.percentage));
-    this.setState({ all: data });
+    const rData = data
+      .sort((a, b) => {
+        if (a.percentage < b.percentage) return 1;
+        if (a.percentage > b.percentage) return -1;
+        return 0;
+      })
+      .filter(g => !isNaN(parseFloat(g.percentage)));
+    const topFive = rData.slice(0, 5);
+    const bottomFive = rData.slice(rData.length - 5);
+    this.setState({ all: rData, topFive, bottomFive });
   }
 
   render() {
@@ -27,19 +38,37 @@ export default class Stats extends Component {
       "percentage"
     ];
     return (
-      <section className="nes-container with-title">
-        <p className="title">Top 5</p>
-        <div className="nes-table-responsive">
-          <MappedTable data={this.state.all} propertyNames={propertyNames} />
-          {/* <ul>
-          {this.state.all.map(el => (
-            <li>
-              {el.name}: {parseFloat(el.percentage)}
-            </li>
-          ))}
-        </ul> */}
-        </div>
-      </section>
+      <>
+        <section className="game-container mx-auto">
+          <section className="w-1/2 mt-16">
+            <h3 className="title">Top 5</h3>
+            <div className="nes-table-responsive">
+              <MappedTable
+                data={this.state.topFive}
+                propertyNames={propertyNames}
+              />
+            </div>
+          </section>
+          <section className="w-1/2 mt-16">
+            <h3 className="title">Bottom 5</h3>
+            <div className="nes-table-responsive">
+              <MappedTable
+                data={this.state.bottomFive}
+                propertyNames={propertyNames}
+              />
+            </div>
+          </section>
+          <section className="w-1/2 mt-16">
+            <h3 className="title">All</h3>
+            <div className="nes-table-responsive">
+              <MappedTable
+                data={this.state.all}
+                propertyNames={propertyNames}
+              />
+            </div>
+          </section>
+        </section>
+      </>
     );
   }
 }
