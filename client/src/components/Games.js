094b1a0f7c630a24bd22336c2d43606f0beed8f1
John Young <john.young@spreadstreet.io>
2019-04-01 12:20:04 -0400

    Finalized search functionality

--- client/src/components/Games.js
+++ client/src/components/Games.js
@@ -6,18 +6,29 @@ import ConnectedGame from "./Game";
 import "./styles/gameInfo.css";
 
 export class Games extends Component {
+  state = {
+    loading: false,
+    value: ''
+  };
+
   componentDidMount() {
     const { dispatch } = this.props;
     dispatch(fetchAllGames());
   }
 
-  render() {
-    const { loading, games } = this.props;
-    let allGames;
-    if (loading || !games) {
-      allGames = <div>Loading...</div>;
-    } else {
-      allGames = games.map(game => {
+  search = () => {
+    this.setState({ loading: true})
+  }
+
+  onChangeHandler = async e => {
+    this.search(e.target.value);
+    this.setState({ value: e.target.value });
+  }
+
+  get renderGames() {
+    let filteredGames = <h1>There's no games</h1>;
+    if (this.props.games) {
+      filteredGames = this.props.games.map(game => {
         const { name, igdb, id } = game;
         const { slug } = igdb;
         const props = {
@@ -27,31 +38,37 @@ export class Games extends Component {
           slug
         };
         return <ConnectedGame key={id} {...props} />;
+      }).filter(game => {
+        return game.props.name.toLowerCase().includes(this.state.value)
       });
     }
+
+    return filteredGames;
+  }
+
+  render() {
     return (
       <section className="game-container mx-auto">
-        <div>
+        <div className="text-center text-2xl mt-16">
           <h1>
-            <i className="nes-logo" />
-            <i className="nes-jp-logo" />
-            <i className="snes-logo" />
-            <i className="snes-jp-logo" />
+            <i className="nes-logo mx-4" />
+            <i className="nes-jp-logo mx-4" />
             Games
-            <i className="nes-icon coin" />
+            <i className="snes-logo mx-4" />
+            <i className="snes-jp-logo mx-4" />
           </h1>
         </div>
         <div className="nes-field mt-16">
-          <label htmlFor="search" />
           <input
-            type="text"
-            id="search"
-            className="nes-input"
-            placeholder="Search hundreds of games in our database!"
+            type='text'
+            className='nes-input'
+            value={this.state.value}
+            onChange={e => this.onChangeHandler(e)}
+            placeholder="Type something to search"
           />
         </div>
         <div className="flex justify-start content-start flex-wrap mt-16">
-          {allGames}
+        {this.renderGames}
         </div>
       </section>
     );
