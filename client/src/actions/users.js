e4ce6d4933d15cbf302bdd016320b64b69f4440a
Evan Guirino <etg624@gmail.com>
2019-04-01 09:48:18 -0700

    displaying all user history data on the back

--- client/src/actions/users.js
+++ client/src/actions/users.js
@@ -1,27 +1,67 @@
-import {SubmissionError} from 'redux-form';
+import { SubmissionError } from 'redux-form';
 
-import {API_BASE_URL} from '../config';
-import {normalizeResponseErrors} from './utils';
+import { API_BASE_URL } from '../config';
+import { normalizeResponseErrors } from './utils';
+import axios from 'axios';
+
+export const GET_USER_REQUEST = 'GET_USER_REQUEST';
+export const getUserRequest = () => ({
+  type: GET_USER_REQUEST
+});
+
+export const GET_USER_SUCCESS = 'GET_USER_SUCCESS';
+export const getUserSuccess = userId => ({
+  type: GET_USER_SUCCESS,
+  userId
+});
+export const GET_USER_ERROR = 'GET_USER_ERROR';
+export const getUserError = error => ({
+  type: GET_USER_SUCCESS,
+  error
+});
+
+export const getUser = userId => (dispatch, getState) => {
+  dispatch(getUserRequest());
+
+  const authToken = getState().auth.authToken;
+  return fetch(`${API_BASE_URL}/users/${userId}/history`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    }
+  })
+    .then(res => {
+      if (!res.ok) {
+        return Promise.resject(res.statusText);
+      }
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      dispatch(getUserSuccess(data));
+    })
+    .catch(err => dispatch(getUserError(err)));
+};
 
 export const registerUser = user => dispatch => {
-    return fetch(`${API_BASE_URL}/users`, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    })
-        .then(res => normalizeResponseErrors(res))
-        .then(res => res.json())
-        .catch(err => {
-            const {reason, message, location} = err;
-            if (reason === 'ValidationError') {
-                // Convert ValidationErrors into SubmissionErrors for Redux Form
-                return Promise.reject(
-                    new SubmissionError({
-                        [location]: message
-                    })
-                );
-            }
-        });
+  return fetch(`${API_BASE_URL}/users`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .catch(err => {
+      const { reason, message, location } = err;
+      if (reason === 'ValidationError') {
+        // Convert ValidationErrors into SubmissionErrors for Redux Form
+        return Promise.reject(
+          new SubmissionError({
+            [location]: message
+          })
+        );
+      }
+    });
 };
