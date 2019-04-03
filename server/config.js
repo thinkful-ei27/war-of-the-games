31b2ef70a303e011ec26a35bbf5dbce4b1230e88
Derek Houck <derek@derekhouck.com>
2019-04-03 09:20:41 -0700

    Improve code coverage and test runtime

--- server/config.js
+++ server/config.js
@@ -4,7 +4,9 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000",
   DATABASE_URL: process.env.DATABASE_URL,
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "mongodb://localhost/war-of-the-games-test",
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || "7d",
   CLOUDINARY_URL: process.env.CLOUDINARY_URL
