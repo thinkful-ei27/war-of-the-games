07278b2c9bb3abb25eddfa2b78d4bb8e568fca9a
John Young <john.young@spreadstreet.io>
2019-04-04 15:38:34 -0400

    adding games to motivations

--- server/db/motivations.js
+++ server/db/motivations.js
@@ -1,18 +1,91 @@
 const motivations = {
   action: {
-    destruction: [],
-    excitement: []
+    destruction: [
+      "Super Smash Bros. Melee",
+      "Battlefield 3",
+      "Counter-Strike",
+      "Call of Duty",
+      "God of War",
+      "Destiny",
+      "Kingdom Hearts 2",
+      "Resident Evil",
+      "League of Legends"
+    ],
+    excitement: [
+      "Grand Theft Auto V",
+      "Battlefield 3",
+      "Destiny",
+      "Call of Duty",
+      "Gears of War",
+      "Halo",
+      "Borderlands 2",
+      "Doom",
+      "God of War",
+      "Counter Strike"
+    ]
   },
   social: {
-    competition: [],
-    community: []
+    competition: [
+      "Counter-Strike: Global Offensive",
+      "Super Smash Bros. Melee",
+      "Dota 2",
+      "League of Legends",
+      "Street Fighter 2",
+      "Heroes of the Storm",
+      "Starcraft 2",
+      "Call of Duty",
+      "Battlefield 3",
+      "FIFA 17"
+    ],
+    community: [
+      "Final Fantasy XIV",
+      "Battlefield 4",
+      "Destiny",
+      "Guild Wars 2",
+      "EverQuest",
+      "League of Legends",
+      "Monster Hunter",
+      "World of Warcraft",
+      "Counter-Strike",
+      "Dota 2"
+    ]
   },
   mastery: {
-    challenge: [],
-    strategy: []
+    challenge: [
+      "Super Smash Bros. Melee",
+      "Devil May Cry 3",
+      "World of Warcraft",
+      "Dark Souls",
+      "Counter-Strike",
+      "Street Fighter 2",
+      "Monster Hunter",
+      "Dota 2",
+      "Starcraft 2",
+      "Warcraft III: Reign of Chaos"
+    ],
+    strategy: [
+      "Europa Universalis IV",
+      "Crusader Kings II",
+      "Civilization 5",
+      "XCOM: Enemy Unknown",
+      "StarCraft",
+      "Fire Emblem",
+      "Age of Empires II: The Age of Kings",
+      "Warcraft III: Reign of Chaos",
+      "Kerbal Space Program"
+    ]
   },
   achievement: {
-    completion: [],
+    completion: [
+      "Final Fantasy IX",
+      "Assassin's Creed",
+      "The Legend of Zelda",
+      "God of War",
+      "Animal Crossing",
+      "The Elder Scrolls V: Skyrim",
+      "Destiny",
+      ""
+    ],
     power: []
   },
   creativity: {
--- server/utils/top.js
+++ server/utils/top.js
@@ -7,7 +7,7 @@ const { DATABASE_URL } = require("../config");
 const Top = require("../models/top");
 
 const fields =
-  "age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,collection,cover,created_at,dlcs,expansions,external_games,first_release_date,follows,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,popularity,pulse_count,rating,rating_count,release_dates,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,time_to_beat,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites";
+  "aggregated_rating,aggregated_rating_count,artworks,category,cover,created_at,first_release_date,follows,franchise,franchises,genres,name,parent_game,platforms,player_perspectives,popularity,pulse_count,rating,rating_count,release_dates,screenshots,similar_games,slug,status,storyline,summary,tags,themes,time_to_beat,total_rating,total_rating_count,updated_at,url,videos,websites";
 
 const keys = process.env.IGDB_KEYS.split(",");
 const randomKey = arr => arr[Math.floor(Math.random() * arr.length)];
@@ -27,7 +27,7 @@ const requestOptions = {
 // Recursively gets all games from the igdb based on rating count
 const getGames = async (ratingCount = 2000, allGames = []) => {
   // Base case
-  if (allGames.length > 100) {
+  if (allGames.length > 1000) {
     return allGames;
   }
 
