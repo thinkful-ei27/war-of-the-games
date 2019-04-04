febe1f20a1b0868f0e43fdcdab38296706f44edc
John Young <john.young@spreadstreet.io>
2019-04-04 14:36:47 -0400

    merged dev

---
+++ server/db/motivations.js
@@ -0,0 +1,26 @@
+const motivations = {
+  action: {
+    destruction: [],
+    excitement: []
+  },
+  social: {
+    competition: [],
+    community: []
+  },
+  mastery: {
+    challenge: [],
+    strategy: []
+  },
+  achievement: {
+    completion: [],
+    power: []
+  },
+  creativity: {
+    discovery: [],
+    design: []
+  },
+  immersion: {
+    fantasy: [],
+    story: []
+  }
+};
