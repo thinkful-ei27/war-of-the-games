bbf794eb7a3eedbfff591b03f5b42949b65d2db3
Derek Houck <derek@derekhouck.com>
2019-04-05 08:49:07 -0700

    Remove history from user response

--- server/test/users.test.js
+++ server/test/users.test.js
@@ -65,7 +65,6 @@ describe("ASYNC Capstone API - Users", () => {
             "username",
             "firstName",
             "lastName",
-            "history",
             "admin",
             "battles"
           );
