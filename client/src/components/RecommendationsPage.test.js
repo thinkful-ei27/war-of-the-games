2aeabd68a2454035ac085a96d938bbedb71dc459
Peter Ramos <peterdramos02@gmail.com>
2019-04-07 17:01:37 -0400

    remove .only from rec-pg test

--- client/src/components/RecommendationsPage.test.js
+++ client/src/components/RecommendationsPage.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import { RecommendationsPage } from "./RecommendationsPage";
 
-describe.only("<RecommendationsPage/>", function() {
+describe("<RecommendationsPage/>", function() {
   it("renders without crashing", () => {
     shallow(<RecommendationsPage />);
   });
