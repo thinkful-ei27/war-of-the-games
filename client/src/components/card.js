a1e4a70512c411d9cff054d4410d4fc0c1e76138
Derek Houck <derek@derekhouck.com>
2019-04-08 14:04:15 -0700

    Update card styles

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -5,23 +5,11 @@ import {
   fetchGames,
   handleVote,
   clearGames,
-  setNonUserVotes,
   clearNonUserVotes
 } from "../actions/gameActions";
-import { incrementVoteCount } from "../local-storage";
 
 export function Card(props) {
-  const {
-    src,
-    alt,
-    name,
-    dispatch,
-    games,
-    id,
-    fetchFeedback,
-    loggedIn,
-    userId
-  } = props;
+  const { src, alt, name, dispatch, games, id, fetchFeedback, userId } = props;
 
   const handleVoteClick = () => {
     dispatch(handleVote(games[0].id, games[1].id, id, userId));
