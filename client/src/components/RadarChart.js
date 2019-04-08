725bd207e61c3b2dc9ad16e260ffece75ee22dbf
Evan Guirino <etg624@gmail.com>
2019-04-08 10:19:58 -0700

    graph rendering on user-profile with user specific data

--- client/src/components/RadarChart.js
+++ client/src/components/RadarChart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   ResponsiveContainer,
   Radar,
@@ -8,41 +8,50 @@ import {
   PolarRadiusAxis,
   Legend
 } from "recharts";
+import { connect } from "react-redux";
+import { getUserMotivationData } from "../actions/users";
 
-const data = [
-  { motivation: "Creativity", value: 62, fullMark: 100 },
-  { motivation: "Mastery", value: 53, fullMark: 100 },
-  { motivation: "Social", value: 57, fullMark: 100 },
-  { motivation: "Action", value: 50, fullMark: 100 },
-  { motivation: "Immersion", value: 72, fullMark: 100 },
-  { motivation: "Achievement", value: 63, fullMark: 100 }
-];
+// const data = [
+//   { motivation: "Creativity", value: 62, fullMark: 100 },
+//   { motivation: "Mastery", value: 53, fullMark: 100 },
+//   { motivation: "Social", value: 57, fullMark: 100 },
+//   { motivation: "Action", value: 50, fullMark: 100 },
+//   { motivation: "Immersion", value: 72, fullMark: 100 },
+//   { motivation: "Achievement", value: 63, fullMark: 100 }
+// ];
 
-export default function MotivationsChart() {
-  return (
-    <div>
-      <ResponsiveContainer>
+class MotivationsChart extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    return dispatch(getUserMotivationData());
+  }
+
+  render() {
+    const { motivations } = this.props;
+    const data = motivations;
+    console.log(motivations);
+    return (
+      <div>
         <RadarChart outerRadius={90} width={730} height={250} data={data}>
           <PolarGrid />
           <PolarAngleAxis dataKey="motivation" />
           <PolarRadiusAxis angle={30} domain={[0, 100]} />
           <Radar
             name="User"
-            dataKey="value"
+            dataKey="percentage"
             stroke="#8884d8"
             fill="#8884d8"
             fillOpacity={0.6}
           />
-          {/* <Radar
-          name="Lily"
-          dataKey="B"
-          stroke="#82ca9d"
-          fill="#82ca9d"
-          fillOpacity={0.6}
-        /> */}
           <Legend />
         </RadarChart>
-      </ResponsiveContainer>
-    </div>
-  );
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  motivations: state.user.motivations
+});
+
+export default connect(mapStateToProps)(MotivationsChart);
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -8,12 +8,16 @@ import {
   POST_USER_ABOUT_ME_SUCCESS,
   GET_USER_ABOUT_ME_SUCCESS,
   GET_USER_ABOUT_ME_ERROR,
-  GET_USER_ABOUT_ME_REQUEST
+  GET_USER_ABOUT_ME_REQUEST,
+  GET_USER_MOTIVATIONS_ERROR,
+  GET_USER_MOTIVATIONS_REQUEST,
+  GET_USER_MOTIVATIONS_SUCCESS
 } from "../actions/users";
 
 const initialState = {
   history: [],
   topHistory: [],
+  motivations: [],
   aboutMe: "",
   loading: false,
   error: null
@@ -22,57 +26,33 @@ const initialState = {
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case GET_USER_REQUEST:
-      return {
-        ...state,
-        loading: true,
-        error: null
-      };
+      return { ...state, loading: true, error: null };
     case GET_USER_SUCCESS:
-      return {
-        ...state,
-        history: [...action.history],
-        loading: false
-      };
+      return { ...state, history: [...action.history], loading: false };
     case GET_USER_ERROR:
-      return {
-        ...state,
-        loading: false,
-        error: action.error
-      };
+      return { ...state, loading: false, error: action.error };
     case GET_USER_TOP_HISTORY_SUCCESS:
-      return {
-        ...state,
-        topHistory: [...action.history]
-      };
+      return { ...state, topHistory: [...action.history] };
     case POST_USER_ABOUT_ME_REQUEST:
-      return {
-        ...state,
-        // loading: true,
-        error: null
-      };
+      return { ...state, error: null };
     case POST_USER_ABOUT_ME_SUCCESS:
+      return { ...state, aboutMe: action.content, loading: false };
+    case POST_USER_ABOUT_ME_ERROR:
+      return { ...state, loading: false, error: null };
+    case GET_USER_ABOUT_ME_REQUEST:
+      return { ...state, loading: true, error: null };
+    case GET_USER_ABOUT_ME_SUCCESS:
+      return { ...state, aboutMe: action.content };
+    case GET_USER_MOTIVATIONS_REQUEST:
+      return { ...state, loading: true, error: null };
+    case GET_USER_MOTIVATIONS_SUCCESS:
       return {
         ...state,
-        aboutMe: action.content,
+        motivations: [...action.data.percentages],
         loading: false
       };
-    case POST_USER_ABOUT_ME_ERROR:
-      return {
-        ...state,
-        loading: false,
-        error: null
-      };
-    case GET_USER_ABOUT_ME_REQUEST:
-      return {
-        ...state,
-        loading: true,
-        error: null
-      };
-    case GET_USER_ABOUT_ME_SUCCESS:
-      return {
-        ...state,
-        aboutMe: action.content
-      };
+    case GET_USER_MOTIVATIONS_ERROR:
+      return { ...state, loading: false, error: action.error };
     default:
       return state;
   }
