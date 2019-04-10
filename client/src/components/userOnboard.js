2138f42603e0f6e6464b0509f7ead51d2da3cb41
Tyler Crabb <tydacrabb@gmail.com>
2019-04-10 09:49:09 -0700

    hopefully final hotfix on onboard

--- client/src/components/userOnboard.js
+++ client/src/components/userOnboard.js
@@ -1,27 +1,12 @@
-import React from "react";
-import { connect } from "react-redux";
-import { setNonUserVotes } from "../actions/gameActions";
-import { nextTest } from "../actions/onboarding";
-import OnboardPropmt from "./onboardPrompt";
-import {
-  setVoteLocalStorageVariable,
-  incrementVoteCount,
-  loadVoteCount
-} from "../local-storage";
-
+import React from 'react';
+import { connect } from 'react-redux';
+import { setNonUserVotes } from '../actions/gameActions'
+import { nextTest } from '../actions/onboarding'
+import OnboardPropmt from './onboardPrompt'
+import { setVoteLocalStorageVariable, saveVoteCount, incrementVoteCount, loadVoteCount } from '../local-storage'
 export class UserOnboard extends React.Component {
-  state = {
-    count: Number(loadVoteCount())
-  };
 
   count = Number(loadVoteCount());
-
-  componentWillMount() {
-    const { dispatch } = this.props;
-    setVoteLocalStorageVariable();
-    const myKey = `test${this.count}`;
-    dispatch(nextTest(myKey));
-  }
 
   handleVote = () => {
     let myKey;
@@ -31,90 +16,72 @@ export class UserOnboard extends React.Component {
     this.props.dispatch(nextTest(myKey))
   }
 
+  componentWillMount() {
+    setVoteLocalStorageVariable()
+    let myKey;
+    myKey = `test${this.count}`;
+    this.props.dispatch(nextTest(myKey))
+  }
+
   render() {
-    const { count } = this.state;
-    const { dispatch, tests } = this.props;
+    let { tests } = this.props;
     let content;
 
-    if (count < 13) {
-      content = (
+    if (this.count < 13) {
+      content =
         <>
           <div className="battle-container">
             <div className="card">
               <div className="title-container">
-                <h1 className="game-title">{tests.showing[0].name}</h1>
+                <h1 className="game-title" >{tests.showing[0].name}</h1>
               </div>
-              <img
-                className="game-img"
-                src={tests.showing[0].coverUrl}
-                alt="test"
-              />
+              <img className="game-img" src={tests.showing[0].coverUrl} alt='test' />
               <button
                 id={tests.showing[0].id}
                 className="nes-btn is-warning"
                 type="button"
                 key={tests.showing[0].id}
-                onClick={e => {
-                  this.handleVote();
-                  dispatch(
-                    setNonUserVotes(
-                      tests.showing[0].id,
-                      tests.showing[1].id,
-                      e.target.id
-                    )
-                  );
+                onClick={(e) => {
+                  this.handleVote()
+                  this.props.dispatch(setNonUserVotes(tests.showing[0].id, tests.showing[1].id, e.target.id))
                 }}
               >
                 Vote
-              </button>
+            </button>
             </div>
             <div className="card-onboard-2">
               <div className="title-container">
-                <h1 className="game-title">{tests.showing[1].name}</h1>
+                <h1 className="game-title" >{tests.showing[1].name}</h1>
               </div>
-              <img
-                className="game-img"
-                src={tests.showing[1].coverUrl}
-                alt="test"
-              />
+              <img className="game-img" src={tests.showing[1].coverUrl} alt='test' />
               <button
                 id={tests.showing[1].id}
                 className="nes-btn is-warning"
                 type="button"
-                onClick={e => {
-                  this.handleVote();
-                  dispatch(
-                    setNonUserVotes(
-                      tests.showing[0].id,
-                      tests.showing[1].id,
-                      e.target.id
-                    )
-                  );
+                onClick={(e) => {
+                  this.handleVote()
+                  this.props.dispatch(setNonUserVotes(tests.showing[0].id, tests.showing[1].id, e.target.id))
                 }}
               >
                 Vote
-              </button>
+            </button>
             </div>
-          </div>
+          </div >
           <div className="onboarding-progress-container">
             <p>Exp:</p>
-            <progress
-              id="onboarding-progress"
-              className="nes-progress is-primary"
-              value={this.count * 10}
-              max="120"
-            />
+            <progress id="onboarding-progress" className="nes-progress is-primary" value={this.count * 10} max="120"></progress>
           </div>
         </>
-      );
-    } else if (this.count >= 13) {
-      content = <OnboardPropmt />;
     }
-    return content;
+    else if (this.count >= 11) {
+      content = <OnboardPropmt />
+
+    }
+    return content
   }
 }
 const mapStateToProps = state => ({
   tests: state.onboard
 });
 
-export default connect(mapStateToProps)(UserOnboard);
+export default connect(mapStateToProps)(UserOnboard)
